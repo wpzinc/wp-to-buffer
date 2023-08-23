3b2580192cdae2bd114da7a28376432603ad0245
Tim Carr <tim@n7studios.co.uk>
2023-08-23 16:03:50 +0100

    3.8.9

--- wp-to-buffer.php
+++ wp-to-buffer.php
@@ -22,7 +22,7 @@ if ( class_exists( 'WP_To_Buffer' ) ) {
 
 // Define Plugin version and build date.
 define( 'WP_TO_BUFFER_PLUGIN_VERSION', '3.8.9' );
-define( 'WP_TO_BUFFER_PLUGIN_BUILD_DATE', '2023-08-24 18:00:00' );
+define( 'WP_TO_BUFFER_PLUGIN_BUILD_DATE', '2023-08-23 18:00:00' );
 
 // Define Plugin paths.
 define( 'WP_TO_BUFFER_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
